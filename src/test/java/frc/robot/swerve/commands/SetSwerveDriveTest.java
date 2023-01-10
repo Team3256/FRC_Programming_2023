49dbf69a57a7b5b61ca87edbaf41b463907d5c5c
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-01-09 18:00:45 -0800

    Fix: unit tests

--- src/test/java/frc/robot/swerve/commands/SetSwerveDriveTest.java
+++ src/test/java/frc/robot/swerve/commands/SetSwerveDriveTest.java
@@ -1,28 +1,33 @@
-package frc.robot.swerve;
+package frc.robot.swerve.commands;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.swerve.SwerveDrive;
 import frc.robot.swerve.commands.TeleopSwerveFieldOriented;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class SetSwerveDriveTest {
+
     public static final double DELTA = 0.05; // acceptable deviation range
     SwerveDrive swerveDrive;
 
     @Test
     public void TestTeleop() {
+
         /*
             Make the suppliers that are accepted by the TeleopSwerveFieldOriented command
          */
+
         Joystick driver = new Joystick(0);
 
         boolean fieldRelative = true;
         boolean openLoop = true;
 
+        Assertions.assertEquals(fieldRelative, true);
     }
 
 
