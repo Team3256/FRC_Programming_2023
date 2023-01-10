ef15d924748af6824977c3c606aca74b2782ab95
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-01-09 17:09:15 -0800

    Fix: removed button config because they are uneccessaryAA

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,11 +26,6 @@ public class RobotContainer {
   /* Controllers */
   private final Joystick driver = new Joystick(0);
 
-  /* Drive Controls */
-  private final int translationAxis = XboxController.Axis.kLeftY.value;
-  private final int strafeAxis = XboxController.Axis.kLeftX.value;
-  private final int rotationAxis = XboxController.Axis.kRightX.value;
-
   /* Driver Buttons */
   private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kA.value);
   private final XboxController driverController = new XboxController(0);
