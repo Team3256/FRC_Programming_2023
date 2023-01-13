20efd95590878524d6f272fbb70187dd9178d8c6
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-12 21:59:47 -0800

    Update Intake to use Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,7 +15,9 @@ public final class Constants {
     public static final double stickDeadband = 0.1;
 
     public static final class IntakeConstants {
-        public static final int motorID = 1;
+        public static final int INTAKE_MOTOR_ID= 1;
+        public static final int INTAKE_FORWARD_SPEED = 1;
+        public static final int INTAKE_BACKWARD_SPEED = 1;
     }
     public static final class SwerveConstants {
         public static final int pigeonID = 1;
--- src/main/java/frc/robot/intake/Intake.java
+++ src/main/java/frc/robot/intake/Intake.java
@@ -3,9 +3,8 @@ package frc.robot.intake;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.IntakeConstants;
 
-import static frc.robot.Constants.IntakeConstants;
+import static frc.robot.Constants.IntakeConstants.*;
 
 
 public class Intake extends SubsystemBase {
@@ -13,18 +12,19 @@ public class Intake extends SubsystemBase {
 	private final TalonFX intakeMotor;
 
 	public Intake() {
-		intakeMotor = new TalonFX(IntakeConstants.motorID);
+		intakeMotor = new TalonFX(INTAKE_MOTOR_ID);
 		off();
+		System.out.println("Intake initialized");
 	}
 
 	public void forward(){
 		System.out.println("Intake forward");
-		intakeMotor.set(ControlMode.PercentOutput, 1);
+		intakeMotor.set(ControlMode.PercentOutput, INTAKE_FORWARD_SPEED);
 	}
 
 	public void backward(){
 		System.out.println("Intake backward");
-		intakeMotor.set(ControlMode.PercentOutput, 1);
+		intakeMotor.set(ControlMode.PercentOutput, INTAKE_BACKWARD_SPEED);
 	}
 
 	public void off(){
