d904b38a912287f0d6fca67febfa2ad9be67b92d
vivek nadig <vivek.nadig@warriorlife.net>
2023-01-13 23:30:08 -0800

    Updated phoenix-latest and added wpilibcommands dependencies

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,24 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRC_Programming_2023";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "dd67a8218cf6fac067305dc2eab04bacf1b1c980";
+  public static final String GIT_DATE = "2023-01-13 23:00:10 PST";
+  public static final String GIT_BRANCH = "feat/add-auto-code";
+  public static final String BUILD_DATE = "2023-01-13 23:06:28 PST";
+  public static final long BUILD_UNIX_TIME = 1673679988753L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
