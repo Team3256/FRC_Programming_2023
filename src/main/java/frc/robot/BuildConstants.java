39091e22054bdd02e424a28618ad7cd2661e4538
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-01-17 21:55:23 -0800

    spotlessApply fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 17;
-  public static final String GIT_SHA = "d99c39b84a0ea3aeb655c80afb8d13fb5ed79e34";
-  public static final String GIT_DATE = "2023-01-16 20:40:45 PST";
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "2f245bdded2d235648cc98993739c65a707fa44c";
+  public static final String GIT_DATE = "2023-01-17 21:52:23 PST";
   public static final String GIT_BRANCH = "feat/vision-prototyping";
-  public static final String BUILD_DATE = "2023-01-17 21:51:55 PST";
-  public static final long BUILD_UNIX_TIME = 1674021115333L;
+  public static final String BUILD_DATE = "2023-01-17 21:55:04 PST";
+  public static final long BUILD_UNIX_TIME = 1674021304953L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
