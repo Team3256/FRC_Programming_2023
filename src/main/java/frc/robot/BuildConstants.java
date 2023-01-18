2f245bdded2d235648cc98993739c65a707fa44c
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-01-17 21:52:23 -0800

    spotlessApply fixed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,24 +1,19 @@
-// Copyright (c) 2023 FRC 3256
-// https://github.com/Team3256
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file at
-// the root directory of this project.
-
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC_Programming_2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "54710f39d02dc0813c42162d3cd08d694eb65349";
-  public static final String GIT_DATE = "2023-01-16 20:16:09 PST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "d99c39b84a0ea3aeb655c80afb8d13fb5ed79e34";
+  public static final String GIT_DATE = "2023-01-16 20:40:45 PST";
   public static final String GIT_BRANCH = "feat/vision-prototyping";
-  public static final String BUILD_DATE = "2023-01-16 20:35:02 PST";
-  public static final long BUILD_UNIX_TIME = 1673930102893L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2023-01-17 21:51:55 PST";
+  public static final long BUILD_UNIX_TIME = 1674021115333L;
+  public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
