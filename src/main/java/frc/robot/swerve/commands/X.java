918c23c4cfbc1dd0cdd2f1edb5545ab2c48e2f75
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-20 22:48:58 -0800

    Make Swerve Final

--- src/main/java/frc/robot/swerve/commands/X.java
+++ src/main/java/frc/robot/swerve/commands/X.java
@@ -1,41 +1,47 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.swerve.commands;
 
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.BotConstants;
 import frc.robot.swerve.SwerveDrive;
 
-public class X extends CommandBase{
-	private SwerveDrive swerveDrive;
+public class X extends CommandBase {
+  private final SwerveDrive swerveDrive;
 
-	public X(SwerveDrive swerveDrive){
-		addRequirements(swerveDrive);
-		this.swerveDrive=swerveDrive;
-	}
+  public X(SwerveDrive swerveDrive) {
+    addRequirements(swerveDrive);
+    this.swerveDrive = swerveDrive;
+  }
 
-	@Override
-	public void initialize() {
-		//calculate the universal X formation inward angle
-		double inwardAngle = Math.tan(BotConstants.length/BotConstants.width);
-		//convert inward angle into specific angles for each swerve module
-		double[] modAngle = {inwardAngle,180-inwardAngle,inwardAngle,180-inwardAngle};
+  @Override
+  public void initialize() {
+    // calculate the universal X formation inward angle
+    double inwardAngle = Math.tan(BotConstants.length / BotConstants.width);
+    // convert inward angle into specific angles for each swerve module
+    double[] modAngle = {inwardAngle, 180 - inwardAngle, inwardAngle, 180 - inwardAngle};
 
-		//set each swerve module to modAngle and stop movement
-		SwerveModuleState[] states = new SwerveModuleState[4];
-		for (int i=0;i<4;i++) states[i] = new SwerveModuleState(0,new Rotation2d(modAngle[i]));
-		swerveDrive.setModuleStates(states);
-	}
+    // set each swerve module to modAngle and stop movement
+    SwerveModuleState[] states = new SwerveModuleState[4];
+    for (int i = 0; i < 4; i++) states[i] = new SwerveModuleState(0, new Rotation2d(modAngle[i]));
+    swerveDrive.setModuleStates(states);
+  }
 
-	@Override
-	public void execute() {}
+  @Override
+  public void execute() {}
 
-	@Override
-	public void end(boolean interrupted) {}
+  @Override
+  public void end(boolean interrupted) {}
 
-	@Override
-	public boolean isFinished() {
-		return true;
-	}
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
 }
