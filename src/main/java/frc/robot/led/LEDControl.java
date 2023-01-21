6fc6f0a700b054586cecd3880a43227a20bcdbbc
Smogless <ryan.niu@warriorlife.net>
2023-01-21 14:58:44 -0800

    ??

--- src/main/java/frc/robot/led/LEDControl.java
+++ src/main/java/frc/robot/led/LEDControl.java
@@ -1,2 +1,11 @@
-package frc.robot.led;public class LEDControl {
+package frc.robot.led;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+
+public class LEDControl {
+    LEDControl led = new AddressableLED(:);
+    public LEDControl() {
+
+    }
+
 }
