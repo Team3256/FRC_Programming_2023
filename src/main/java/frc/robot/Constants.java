8f8c6ea425acebdfadc099596d9c9bb197e56b1c
Advay Balakrishnan <a.balakrishnan@warriorlife.net>
2023-01-20 17:49:50 -0800

    spotlessApply fixed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,14 +23,14 @@ public final class Constants {
 
   public static final Mode currentMode = Mode.REAL;
 
-  public static enum Mode {
+  public enum Mode {
     REAL,
     SIM,
     REPLAY
   }
 
   public static final class SwerveConstants {
-    public static final int pigeonID = 9;
+    public static final int pigeonID = 1;
     public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
@@ -114,9 +114,9 @@ public final class Constants {
     /* Front Left Module - Module 0 */
     // TODO: Reassign Module Motor and Cancoder ID's to the actual ones in Phoenix tuner
     public static final class Mod0 {
-      public static final int driveMotorID = 13;
-      public static final int angleMotorID = 11;
-      public static final int canCoderID = 12;
+      public static final int driveMotorID = 3;
+      public static final int angleMotorID = 4;
+      public static final int canCoderID = 2;
       public static final double angleOffset = Units.radiansToDegrees(2.977361);
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -124,9 +124,9 @@ public final class Constants {
 
     /* Front Right Module - Module 1 */
     public static final class Mod1 {
-      public static final int driveMotorID = 7;
-      public static final int angleMotorID = 5;
-      public static final int canCoderID = 6;
+      public static final int driveMotorID = 6;
+      public static final int angleMotorID = 7;
+      public static final int canCoderID = 5;
       public static final double angleOffset = Units.radiansToDegrees(0.846566);
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -134,9 +134,9 @@ public final class Constants {
 
     /* Back Left Module - Module 2 */
     public static final class Mod2 {
-      public static final int driveMotorID = 16;
-      public static final int angleMotorID = 14;
-      public static final int canCoderID = 15;
+      public static final int driveMotorID = 9;
+      public static final int angleMotorID = 10;
+      public static final int canCoderID = 8;
       public static final double angleOffset = Units.radiansToDegrees(3.166089);
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -144,9 +144,9 @@ public final class Constants {
 
     /* Back Right Module - Module 3 */
     public static final class Mod3 {
-      public static final int driveMotorID = 4;
-      public static final int angleMotorID = 2;
-      public static final int canCoderID = 3;
+      public static final int driveMotorID = 12;
+      public static final int angleMotorID = 13;
+      public static final int canCoderID = 11;
       public static final double angleOffset = Units.radiansToDegrees(4.067925);
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
