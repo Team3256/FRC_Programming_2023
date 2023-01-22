bb8fd155bb0e963de0120de1641ff0292736d0b9
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-21 21:59:20 -0800

    Create LEDAllSectionSetPattern.java

---
+++ src/main/java/frc/robot/led/commands/LEDAllSectionSetPattern.java
@@ -0,0 +1,26 @@
+package frc.robot.led.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.led.LED;
+import frc.robot.led.patternBases.LEDPattern;
+
+public class LEDAllSectionSetPattern extends CommandBase{
+	private final LED LEDSubsystem;
+	private final LEDPattern pattern;
+
+	public LEDAllSectionSetPattern(LED LEDSubsystem,LEDPattern pattern){
+		addRequirements(LEDSubsystem);
+		this.LEDSubsystem = LEDSubsystem;
+		this.pattern=pattern;
+	}
+
+	@Override
+	public void initialize() {
+		LEDSubsystem.bulkSet(pattern);
+	}
+
+	@Override
+	public boolean isFinished() {
+		return true;
+	}
+}
