c8ab38e7460bcc0092b603e3f1ba0da87777791d
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-21 23:34:14 -0800

    Update AnimatedPattern.java

--- src/main/java/frc/robot/led/patternBases/AnimatedPattern.java
+++ src/main/java/frc/robot/led/patternBases/AnimatedPattern.java
@@ -1,4 +1,32 @@
 package frc.robot.led.patternBases;
 
-public class AnimatedPattern{
+import frc.robot.drivers.Color;
+
+public class AnimatedPattern extends LEDPattern {
+	LEDPattern[] eventList;
+	int ticks;
+	int tick;
+
+	public AnimatedPattern(){
+		super();
+		ticks=1;
+		eventList = new LEDPattern[ticks];
+	}
+
+	public AnimatedPattern(int ticks){
+		super();
+		this.ticks=ticks;
+		eventList = new LEDPattern[ticks];
+	}
+
+	@Override
+	public Color[] getPattern(){
+		tick=(tick+1)%ticks;
+		pattern = eventList[tick].getPattern();
+		return pattern;
+	}
+
+	public void setEvent(int tick, LEDPattern event){
+		eventList[tick]=event;
+	}
 }
