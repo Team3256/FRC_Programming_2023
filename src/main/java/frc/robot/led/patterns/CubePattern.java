c0e4e1b6f962c712755efe952e4105e28d819888
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-21 19:03:31 -0800

    Update CubePattern.java

--- src/main/java/frc/robot/led/patterns/CubePattern.java
+++ src/main/java/frc/robot/led/patterns/CubePattern.java
@@ -7,25 +7,18 @@
 
 package frc.robot.led.patterns;
 
+import frc.robot.Constants.LEDConstants;
 import frc.robot.drivers.Color;
 
 /**
- * first half of pattern displays color of first ball in the robot second half of pattern displays
- * color of second ball in the robot (red or blue) pattern will take up 0 to 49 percentage range
+ * Full Purple
  */
 public class CubePattern extends LEDPattern {
+  public CubePattern(Color[] totalPattern) {super(0, 99, totalPattern);}
 
-  public CubePattern(Color[] totalPattern) {
-    // percentage range that pattern is allocated to
-    super(0, 49, totalPattern);
-  }
-
-  // event driven/state driven updates
   @Override
   public void update() {
     super.update();
-    // set colouring of pattern
-    setRange(0, 24, new Color(255, 0, 0));
-    setRange(25, 49, new Color(0, 0, 255));
+    setRange(0, 99, LEDConstants.purple);
   }
 }
