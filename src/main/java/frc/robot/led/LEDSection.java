9950d8568b60f71dfe5f3c273ca6d3b3e7c10bcc
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-21 21:32:13 -0800

    Rename Container to Section

--- src/main/java/frc/robot/led/LEDSection.java
+++ src/main/java/frc/robot/led/LEDSection.java
@@ -10,29 +10,32 @@ package frc.robot.led;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import frc.robot.drivers.Color;
 
+import frc.robot.led.patternBases.LEDPattern;
+
 /**
  * displays a pattern onto section of LED
  */
-public class LEDContainer {
+public class LEDSection{
   int start;
   int end;
   int length;
 
-  public LEDContainer(int start, int end) {
+  public LEDSection(int start,int end) {
     this.start = start;
     this.end = end;
     length = end - start + 1;
   }
 
-  public void display(Color[] totalPattern, AddressableLEDBuffer buffer) {
+  public void writeToBuffer(LEDPattern pattern,AddressableLEDBuffer buffer) {
     // loop through every percent
-    for (int i = 0; i < 100; i++) {
+    for (int percent = 1; percent <= 100; percent++) {
       // convert percentage to pixel
-      int pixel = start + (length * i / 100);
+      int pixel = start + (length * percent / 100);
       // finesse out of bound errors
       if (pixel < 0 || pixel >= buffer.getLength()) continue;
       // set the buffer pixel to the color
-      Color color = totalPattern[i];
+
+      Color color = pattern.get(percent);
       buffer.setRGB(pixel, color.R, color.G, color.B);
     }
   }
