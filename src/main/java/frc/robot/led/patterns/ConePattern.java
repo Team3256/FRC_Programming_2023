5954053adcf6cbd95da7d517498914779dd2bac7
3LucasZ <lucas.j.zheng@gmail.com>
2023-01-21 17:42:31 -0800

    Add Cube Pattern, Cone Pattern

---
+++ src/main/java/frc/robot/led/patterns/ConePattern.java
@@ -0,0 +1,26 @@
+package frc.robot.led.patterns;
+
+import frc.robot.drivers.Color;
+
+/**
+ * first half of pattern displays color of first ball in the robot
+ * second half of pattern displays color of second ball in the robot
+ * (red or blue)
+ * pattern will take up 0 to 49 percentage range
+ */
+public class ConePattern extends LEDPattern {
+
+    public ConePattern(Color[] totalPattern) {
+        //percentage range that pattern is allocated to
+        super(0, 49, totalPattern);
+    }
+
+    //event driven/state driven updates
+    @Override
+    public void update(){
+        super.update();
+        //set colouring of pattern
+        setRange(0,24,new Color(255,0,0));
+        setRange(25,49, new Color(0,0,255));
+    }
+}
