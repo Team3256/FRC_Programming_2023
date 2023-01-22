8ad36ee3fcba743b54bcc97d627007f040917486
samuel <116925676+memori034@users.noreply.github.com>
2023-01-21 17:36:23 -0800

    Add full button list and functions as well as their respective positions on the image

--- src/main/java/frc/robot/swerve/helpers/ControllerLabelling.java
+++ src/main/java/frc/robot/swerve/helpers/ControllerLabelling.java
@@ -19,10 +19,25 @@ public class ControllerLabelling {
         ImageIO.read(new URL("https://m.media-amazon.com/images/I/41LO2OX6pRL.jpg")); // URL here
 
     Graphics g = image.getGraphics();
-    g.setFont(g.getFont().deriveFont(18f));
-    g.drawString("Swerve Translation\n(Left Stick)", -115, 87); // for left stick
-    g.drawString("Swerve Rotation\n(Right Stick)", 62, 18); // for right stick
-    g.drawString("Reset Gyro\n(A)", 117, 55); // for A button
+    g.setFont(g.getFont().deriveFont(16f));
+
+    g.drawString("LeftBumper: Auto Align Turrent & Hood\nLeft Trigger: Exhaust/Unjam", -230, 400); 
+    // For left bumper/trigger
+    g.drawString("Right Bumper: Rev Up Flywheel\nRight Trigger: Intake/Unjam", -230, 400); 
+    // For right bumper/trigger
+    g.drawString("Swerve Translation\n(Left Stick)", -230, 170); 
+    // For left stick
+    g.drawString("Swerve Rotation\n(Right Stick)", 122, 32); 
+    // For right stick
+    g.drawString("A: Reset Gyro\nB: Unassigned\nX: Toggle Azimuth Control\nY: Unassigned", 237, 168 ); 
+    // For buttons A, B, X, Y
+    g.drawString("Up: Feeder Manual Forward\nDown: Feeder Mannual Back\nLeft: Unassigned\nRight: Unassigned", -119, 22); 
+    // For D-pad direction buttons
+    g.drawString("Intake Raise\n(Back Button)", 0, 0); // Button does not exist on controller
+    // For back button
+    g.drawString("Intake Drop\n(Start Button", 0, 0); // Button does not exist on controller
+    // For start button
+    
     g.dispose();
 
     ImageIO.write(image, "png", new File("test.png"));
