d7534ffe0688ae4d5bc3b7e48540b51a98dbbee9
3LucasZ <lucas.zheng@warriorlife.net>
2023-01-26 17:23:16 -0800

    Create Loggable.java

---
+++ src/main/java/frc/robot/drivers/Loggable.java
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.drivers;
+
+public interface Loggable {
+  public void startLog();
+
+  public void periodicLog();
+}
