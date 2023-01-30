4f221ba88ea597b601bd91c62fb6cdce72272bb4
samuel <116925676+memori034@users.noreply.github.com>
2023-01-30 15:46:11 -0800

    Update command names to be more descriptive of their functions Add run scheduler function and integrate with methods

--- src/test/ArmUnitTest.java
+++ src/test/ArmUnitTest.java
@@ -5,10 +5,12 @@
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
+// Use command scheduler function
 package frc.robot.arm;
 
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.arm.commands.SetArmFromPID;
 import frc.robot.arm.commands.SetArmFromVoltage;
@@ -38,31 +40,31 @@ public class ArmUnitTest {
   // SetArmFromVoltage tests
   @Test
   public void setVoltageTo5Volts() {
-    SetArmFromVoltage setVoltageTo5Command = new SetArmFromVoltage(armSubsystem, 5.5);
+    SetArmFromVoltage setVoltageTo5Command = new SetArmFromVoltage(armSubsystem, 5.0);
     setVoltageTo5Command.schedule();
-    runScheduler();
+    runScheduler(1);
     DriverStationSim.setEnabled(true);
     assertEquals("Set voltage to 0", ArmSubsystem.getVoltage(), 5.0, DELTA);
   }
 
   @Test
-  public void setVoltageRoutine() {
-    SetArmFromVoltage setVoltageTo5Volts = new SetArmFromVoltage(armSubsystem, 0.5);
+  public void setVoltageRoutine5() {
+    SetArmFromVoltage setVoltageTo5Volts = new SetArmFromVoltage(armSubsystem, 5.0);
     setVoltageTo5Volts.schedule();
-    runScheduler();
+    runScheduler(1);
     DriverStationSim.setEnabled(true);
     assertEquals("Set voltage to 5", ArmSubsystem.getVoltage(), 5.0, DELTA);
     wait(1500);
 
     SetArmFromVoltage setVoltageTo0Volts = new SetArmFromVoltage(armSubsystem, 0.0);
     setVoltageTo0Volts.schedule();
-    runScheduler();
+    runScheduler(1);
     assertEquals("Set voltage to 0", ArmSubsystem.getVoltage(), 0.0, DELTA);
     wait(1500);
 
-    SetArmFromVoltage setVoltageTo5Volts = new SetArmFromVoltage(armSubsystem, 0.5);
-    setVoltageTo5Volts.schedule();
-    runScheduler();
+    SetArmFromVoltage setVoltageBackTo5Volts = new SetArmFromVoltage(armSubsystem, 5.0);
+    setVoltageBackTo5Volts.schedule();
+    runScheduler(1);
     assertEquals("Set voltage to 5", ArmSubsystem.getVoltage(), 5.0, DELTA);
     wait(1000);
   }
@@ -72,7 +74,7 @@ public class ArmUnitTest {
   public void setArmFromPID() {
     SetArmFromPID setArmFromPIDCommand = new SetArmFromPID(armSubsystem, 1200);
     setArmFromPIDCommand.schedule();
-    runScheduler();
+    runScheduler(1);
     DriverStationSim.setEnabled(true);
     assertEquals(
         "Set angular velocity to 1200 RPM", ArmSubsystem.getAngularVelocityRPM(), 1200, DELTA);
@@ -80,26 +82,38 @@ public class ArmUnitTest {
 
   @Test
   public void setArmFromPIDRoutine() {
-    SetArmFromPID setArmFromPIDCommand1 = new SetArmFromPID(armSubsystem, 600);
-    // use assert true to compare initialized value with actual value
-    setArmFromPIDCommand1.schedule();
-    runScheduler(0);
+    SetArmFromPID setArmPIDTo600 = new SetArmFromPID(armSubsystem, 600);
+    setArmPIDTo600.schedule();
+    runScheduler(1.5);
     DriverStationSim.setEnabled(true);
     assertEquals(
         "Set angular velocity to 600 RPM", armSubsystem.getAngularVelocityRPM(), 600, DELTA);
     wait(1000);
 
-    SetArmFromPID setArmFromPIDCommand2 = new SetArmFromPID(armSubsystem, 0);
-    setArmFromPIDCommand2.schedule();
-    runScheduler();
+    SetArmFromPID setArmPIDTo0 = new SetArmFromPID(armSubsystem, 0);
+    setArmPIDTo0.schedule();
+    runScheduler(1);
     assertEquals("Set angular velocity to 0 RPM", armSubsystem.getAngularVelocityRPM(), 0, DELTA);
     wait(1000);
 
-    SetArmFromPID setArmFromPIDCommand3 = new SetArmFromPID(armSubsystem, 1200);
-    setArmFromPIDCommand3.schedule();
-    runScheduler();
+    SetArmFromPID setArmPIDTo1200 = new SetArmFromPID(armSubsystem, 1200);
+    setArmPIDTo1200.schedule();
+    runScheduler(1.5);
     assertEquals(
         "Set angular velocity to 1200 RPM", armSubsystem.getAngularVelocityRPM(), 1200, DELTA);
-    setArmFromPIDCommand3.end();
+    setArmPIDTo1200.end();
+  }
+
+  private static void runScheduler(double seconds) {
+    try {
+      for (int i = 0; i < seconds * 1000 / 20; ++i) {
+        com.ctre.phoenix.unmanaged.Unmanaged.feedEnable(100); 
+        // TODO: Change to motor the arm is using
+        CommandScheduler.getInstance().run();
+        Thread.sleep(20);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
