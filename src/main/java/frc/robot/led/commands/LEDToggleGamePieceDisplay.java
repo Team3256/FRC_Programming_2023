89afb78c9f13e9826fc31f9aca39e61ccdadd487
3LucasZ <lucas.j.zheng@gmail.com>
2023-02-04 14:34:11 -0800

    Create LEDToggleGamePieceDisplay.java

---
+++ src/main/java/frc/robot/led/commands/LEDToggleGamePieceDisplay.java
@@ -0,0 +1,30 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.led.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.led.LEDStrip;
+
+public class LEDToggleGamePieceDisplay extends CommandBase {
+  private final LEDStrip ledStrip;
+
+  public LEDToggleGamePieceDisplay(LEDStrip ledStrip) {
+    addRequirements(ledStrip);
+    this.ledStrip = ledStrip;
+  }
+
+  @Override
+  public void initialize() {
+    ledStrip.toggleGamePiece();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
