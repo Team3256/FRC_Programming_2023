303565398b7f5a2e459c65de931295b4bb3b0b07
3LucasZ <lucas.j.zheng@gmail.com>
2023-02-04 14:35:42 -0800

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,6 @@ import static frc.robot.swerve.SwerveConstants.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.drivers.CANTestable;
 import frc.robot.intake.Intake;
@@ -20,8 +19,7 @@ import frc.robot.intake.commands.IntakeCone;
 import frc.robot.intake.commands.IntakeCube;
 import frc.robot.led.LEDStrip;
 import frc.robot.led.commands.LEDSetAllSectionsPattern;
-import frc.robot.led.patterns.BlinkingConePattern;
-import frc.robot.led.patterns.BlinkingCubePattern;
+import frc.robot.led.commands.LEDToggleGamePieceDisplay;
 import frc.robot.led.patterns.ColorChaseBluePattern;
 import frc.robot.swerve.SwerveDrive;
 import frc.robot.swerve.commands.TeleopSwerve;
@@ -113,13 +111,7 @@ public class RobotContainer {
 
   public void configureLEDStrip() {
     ledStrip = new LEDStrip(0, new int[] {100});
-    driver
-        .a()
-        .onTrue(
-            new SequentialCommandGroup(
-                new LEDSetAllSectionsPattern(
-                    ledStrip, cubePiece ? new BlinkingCubePattern() : new BlinkingConePattern()),
-                new InstantCommand(() -> cubePiece = !cubePiece)));
+    driver.a().onTrue(new LEDToggleGamePieceDisplay(ledStrip));
     driver.b().onTrue(new LEDSetAllSectionsPattern(ledStrip, new ColorChaseBluePattern()));
   }
 
