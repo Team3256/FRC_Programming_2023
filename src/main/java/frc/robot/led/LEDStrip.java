1ca5bf6a60d9ee723f918ea0f7cee8c8985baa78
3LucasZ <lucas.zheng@warriorlife.net>
2023-02-03 20:46:17 -0800

    Update LEDStrip.java

--- src/main/java/frc/robot/led/LEDStrip.java
+++ src/main/java/frc/robot/led/LEDStrip.java
@@ -13,52 +13,42 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.led.patternBases.LEDPattern;
 import java.util.Arrays;
 
-/**
- * allocates sections of a full LED strip to multiple strips that display the same image information
- * is stored in percentage instead of pixel ledContainers stores the different sections of the full
- * LED strip ledPatterns stores the different patterns that are displayed totalPattern is the
- * information from ledPatterns concatenated led and buffer are standard for controlling the led
- * periodic will be called in Robot periodic it updates totalPattern, updates ledContainers, and
- * then sets the led
- */
 public class LEDStrip extends SubsystemBase {
   private final int length;
   private final int sections;
-  private final LEDSection[] ledSections;
-  private final AddressableLED led;
+  private final LEDSection[] LEDSections;
+  private final AddressableLED addressableLED;
   private final AddressableLEDBuffer buffer;
 
   /**
    * Addressable LED strip controller.
    *
    * @param port PWM port on the RoboRio
-   * @param ledSections An array of LED section lengths
+   * @param LEDSectionLengths An array of LED section lengths
    */
-  public LEDStrip(int port, int[] ledSectionLengths) {
-    // initialize led and buffer
-    length = Arrays.stream(ledSectionLengths).sum();
-    sections = ledSectionLengths.length;
-    ledSections = new LEDSection[sections];
-
-    led = new AddressableLED(port);
-    led.setLength(length);
+  public LEDStrip(int port, int[] LEDSectionLengths) {
+    length = Arrays.stream(LEDSectionLengths).sum();
+    sections = LEDSectionLengths.length;
+    LEDSections= new LEDSection[sections];
+    addressableLED= new AddressableLED(port);
+    addressableLED.setLength(length);
     buffer = new AddressableLEDBuffer(length);
 
-    // initialize led containers sequentially
+    // initialize LED sections
     int cur = 0;
     for (int i = 0; i < sections; i++) {
-      ledSections[i] = new LEDSection(cur, cur + ledSectionLengths[i] - 1);
-      cur += ledSectionLengths[i];
+      LEDSections[i] = new LEDSection(cur, cur + LEDSectionLengths[i] - 1);
+      cur += LEDSectionLengths[i];
     }
 
-    // turn on led to default
-    led.start();
-    led.setData(buffer);
+    // start LEDs
+    addressableLED.start();
+    addressableLED.setData(buffer);
   }
 
   public void set(int sectionId, LEDPattern pattern) {
     // set a specific section's buffer to a pattern
-    ledSections[sectionId].setPattern(pattern);
+    LEDSections[sectionId].setLEDPattern(pattern);
   }
 
   public void setAll(LEDPattern pattern) {
@@ -69,10 +59,10 @@ public class LEDStrip extends SubsystemBase {
   }
 
   public void periodic() {
-    // display the pattern in each container's buffer
-    for (LEDSection section : ledSections) {
+    // set each container's display to the pattern in it's buffer
+    for (LEDSection section : LEDSections) {
       section.writeToBuffer(buffer);
     }
-    led.setData(buffer);
+    addressableLED.setData(buffer);
   }
 }
