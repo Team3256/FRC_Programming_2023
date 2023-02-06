3086473e72159285acbf8403b64b0a5d65a980ff
samuel <116925676+memori034@users.noreply.github.com>
2023-02-06 15:23:15 -0800

    Zero offset values to prepare for testing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -115,7 +115,7 @@ public final class Constants {
       public static final int driveMotorID = 3;
       public static final int angleMotorID = 4;
       public static final int canCoderID = 2;
-      public static final double angleOffset = 531.6064455; // 531 or 171 (ziptide constants)
+      public static final double angleOffset = 0; // 531 or 171 (ziptide constants)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
@@ -124,7 +124,7 @@ public final class Constants {
       public static final int driveMotorID = 6;
       public static final int angleMotorID = 7;
       public static final int canCoderID = 5;
-      public static final double angleOffset = 48.691406; // (ziptide constants)
+      public static final double angleOffset = 0; // (ziptide constants)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
@@ -133,7 +133,7 @@ public final class Constants {
       public static final int driveMotorID = 9;
       public static final int angleMotorID = 10;
       public static final int canCoderID = 8;
-      public static final double angleOffset = 174.770508; // (ziptide constants)
+      public static final double angleOffset = 0; // (ziptide constants)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
@@ -142,7 +142,7 @@ public final class Constants {
       public static final int driveMotorID = 12;
       public static final int angleMotorID = 13;
       public static final int canCoderID = 11;
-      public static final double angleOffset = 233.0419925; // (ziptide constants)
+      public static final double angleOffset = 0; // (ziptide constants)
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
