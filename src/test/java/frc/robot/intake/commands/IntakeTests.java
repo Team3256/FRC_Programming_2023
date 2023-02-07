86cb5de9862f18b552e76cca43b49801ebf15983
Smogless <92347357+Smogless@users.noreply.github.com>
2023-02-06 22:16:54 -0800

    made a superclass for unit tests (has setup and run scheduler in it) (#125)
    * made a superclass for unit tests (has setup and run scheduler in it)
    
    * Rename UnitTestSuperClass to TestBase
    
    * refactored/fixed UnitTestBase superclass
    
    * Fix test superclass
    
    ---------
    
    Co-authored-by: Aditya Pawar <34043950+adityapawar1@users.noreply.github.com>
    Co-authored-by: samuel <116925676+memori034@users.noreply.github.com>
    Co-authored-by: Aditya Pawar <aditya.pawar@warriorlife.net>

--- src/test/java/frc/robot/intake/commands/IntakeTests.java
+++ src/test/java/frc/robot/intake/commands/IntakeTests.java
@@ -10,23 +10,19 @@ package frc.robot.intake.commands;
 import static frc.robot.intake.IntakeConstants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-import edu.wpi.first.hal.HAL;
-import edu.wpi.first.wpilibj.simulation.DriverStationSim;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.UnitTestBase;
 import frc.robot.intake.Intake;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-public class IntakeTests {
+public class IntakeTests extends UnitTestBase {
   public final double DELTA = 0.05;
 
   private static Intake intakeSubsystem;
 
   @BeforeAll
   public static void setup() {
-    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
-    CommandScheduler.getInstance().enable();
-    DriverStationSim.setEnabled(true);
+    UnitTestBase.setup();
     intakeSubsystem = new Intake();
   }
 
@@ -34,7 +30,7 @@ public class IntakeTests {
   public void testIntakeCube() {
     IntakeCube command = new IntakeCube(intakeSubsystem);
     command.initialize();
-    runScheduler(1);
+    runScheduler(1, command, intakeSubsystem);
 
     double velocity = intakeSubsystem.getIntakeSpeed();
     assertEquals(kIntakeCubeSpeed, velocity, DELTA);
@@ -44,21 +40,9 @@ public class IntakeTests {
   public void testIntakeCone() {
     IntakeCone command = new IntakeCone(intakeSubsystem);
     command.initialize();
-    runScheduler(1);
+    runScheduler(1, command, intakeSubsystem);
 
     double velocity = intakeSubsystem.getIntakeSpeed();
     assertEquals(kIntakeConeSpeed, velocity, DELTA);
-  }
-
-  private static void runScheduler(double seconds) {
-    try {
-      for (int i = 0; i < seconds * 1000 / 20; ++i) {
-        com.ctre.phoenix.unmanaged.Unmanaged.feedEnable(100);
-        CommandScheduler.getInstance().run();
-        Thread.sleep(20);
-      }
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
   }
 }
