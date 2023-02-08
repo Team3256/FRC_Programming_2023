791c6cb1661e41c1ad2be274b50468ad7f4b91b5
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-02-08 14:23:16 -0800

    Run spotless

--- src/main/java/frc/robot/swerve/SwerveDrive.java
+++ src/main/java/frc/robot/swerve/SwerveDrive.java
@@ -7,6 +7,9 @@
 
 package frc.robot.swerve;
 
+import static frc.robot.Constants.SwerveConstants.*;
+import static frc.robot.Constants.VisionConstants.*;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.sensors.PigeonIMU;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
@@ -25,9 +28,6 @@ import frc.robot.drivers.CANTestable;
 import frc.robot.limelight.Limelight;
 import frc.robot.swerve.helpers.SwerveModule;
 import org.littletonrobotics.junction.Logger;
-
-import static frc.robot.Constants.SwerveConstants.*;
-import static frc.robot.Constants.VisionConstants.*;
 
 public class SwerveDrive extends SubsystemBase implements CANTestable {
   private final SwerveModule frontLeftModule = new SwerveModule(0, FrontLeft.constants);
