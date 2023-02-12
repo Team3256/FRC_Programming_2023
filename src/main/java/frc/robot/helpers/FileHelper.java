705388096117df9660f0f2d2ad7ab0731de8028e
3LucasZ <lucas.j.zheng@gmail.com>
2023-02-11 16:44:56 -0800

    Create FileHelper.java

---
+++ src/main/java/frc/robot/helpers/FileHelper.java
@@ -0,0 +1,27 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.helpers;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.json.simple.JSONObject;
+
+public class FileHelper {
+  public static void saveJson(JSONObject json, String path) {
+    try {
+      PrintWriter pw = new PrintWriter(new FileWriter(path));
+
+      json.writeJSONString(pw);
+      pw.close();
+    } catch (IOException e) {
+      System.out.println("IO error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
