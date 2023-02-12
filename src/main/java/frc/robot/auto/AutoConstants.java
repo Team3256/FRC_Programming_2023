765eb0ab1629b39a19ba5d37e21deb161bc3eae5
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-02-11 23:34:41 -0800

    Clean Path.java

--- src/main/java/frc/robot/auto/AutoConstants.java
+++ src/main/java/frc/robot/auto/AutoConstants.java
@@ -26,9 +26,8 @@ public final class AutoConstants {
   public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
   // Constraint for the motion profiled robot angle controller
-  public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
-      new TrapezoidProfile.Constraints(
-          kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+  public static final TrapezoidProfile.Constraints kThetaControllerConstraints = new TrapezoidProfile.Constraints(
+      kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
 
   /* Auto translation constants */
   public static double kAutoXTranslationP = 2.2;
@@ -45,21 +44,19 @@ public final class AutoConstants {
   public static double kAutoThetaControllerP = 5.4;
   public static double kAutoThetaControllerI = 0.02;
   public static double kAutoThetaControllerD = 1.5;
-  public static TrapezoidProfile.Constraints kAutoThetaControllerConstraints =
-      new TrapezoidProfile.Constraints(
-          AutoConstants.kMaxAngularSpeedRadiansPerSecond,
-          AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared);
+  public static TrapezoidProfile.Constraints kAutoThetaControllerConstraints = new TrapezoidProfile.Constraints(
+      AutoConstants.kMaxAngularSpeedRadiansPerSecond,
+      AutoConstants.kMaxAngularSpeedRadiansPerSecondSquared);
 
   public static final class DynamicPathGenerationConstants {
     public static final double kCollisionBuffer = Units.inchesToMeters(10);
-    public static final double kControlPointScalar = 0.5;
-    public static final Translation2d[][] kChargingStationSegments =
-        new Translation2d[][] {
-          {kChargingStationCorners[0], kChargingStationCorners[1]},
-          {kChargingStationCorners[0], kChargingStationCorners[2]},
-          {kChargingStationCorners[1], kChargingStationCorners[3]},
-          {kChargingStationCorners[2], kChargingStationCorners[3]},
-        };
+    public static final double kControlPointScalar = 0.2;
+    public static final Translation2d[][] kChargingStationSegments = new Translation2d[][] {
+        { kChargingStationCorners[0], kChargingStationCorners[1] },
+        { kChargingStationCorners[0], kChargingStationCorners[2] },
+        { kChargingStationCorners[1], kChargingStationCorners[3] },
+        { kChargingStationCorners[2], kChargingStationCorners[3] },
+    };
 
     // Graph represnted below in (x, y)
     // <-> Represent edges
@@ -68,36 +65,34 @@ public final class AutoConstants {
     // 5.9, 4.3 <-> 4.5, 4.65 <-> 3.3, 4.65 <-> 2.2, 4.4 <-> 2.2, 2.7
     // |
     // 5.9, 1.3 <-> 4.5, 0.70 <-> 3.3, 0.70 <-> 2.2, 1.0 <-> 2.2, 2.7
-    public static final Pose2d poseIndexes[] =
-        new Pose2d[] {
-          new Pose2d(new Translation2d(5.9, 4.3), Rotation2d.fromDegrees(-35)),
-          new Pose2d(new Translation2d(4.5, 4.65), Rotation2d.fromDegrees(0)),
-          new Pose2d(new Translation2d(3.3, 4.65), Rotation2d.fromDegrees(-170)),
-          new Pose2d(new Translation2d(2.2, 4.4), Rotation2d.fromDegrees(-125)),
-          new Pose2d(new Translation2d(2.2, 2.7), Rotation2d.fromDegrees(90)),
-          new Pose2d(new Translation2d(5.9, 1.3), Rotation2d.fromDegrees(40)),
-          new Pose2d(new Translation2d(4.5, 0.7), Rotation2d.fromDegrees(0)),
-          new Pose2d(new Translation2d(3.3, 0.7), Rotation2d.fromDegrees(-10)),
-          new Pose2d(new Translation2d(2.2, 1.0), Rotation2d.fromDegrees(-80)),
-        };
+    public static final Pose2d poseIndexes[] = new Pose2d[] {
+        new Pose2d(new Translation2d(5.9, 4.3), Rotation2d.fromDegrees(-35)),
+        new Pose2d(new Translation2d(4.5, 4.65), Rotation2d.fromDegrees(0)),
+        new Pose2d(new Translation2d(3.3, 4.65), Rotation2d.fromDegrees(-170)),
+        new Pose2d(new Translation2d(2.2, 4.4), Rotation2d.fromDegrees(-125)),
+        new Pose2d(new Translation2d(2.2, 2.7), Rotation2d.fromDegrees(90)),
+        new Pose2d(new Translation2d(5.9, 1.3), Rotation2d.fromDegrees(40)),
+        new Pose2d(new Translation2d(4.5, 0.7), Rotation2d.fromDegrees(0)),
+        new Pose2d(new Translation2d(3.3, 0.7), Rotation2d.fromDegrees(-10)),
+        new Pose2d(new Translation2d(2.2, 1.0), Rotation2d.fromDegrees(-80)),
+    };
 
     // Will be filled by start and end pose
-    private static final double[] emptyRow = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private static final double[] emptyRow = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-    public static final double[][] origGraph =
-        new double[][] {
-          {0.0000, 1.4431, 0.0000, 0.0000, 0.0000, 3.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000},
-          {1.4431, 0.0000, 1.2000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000},
-          {0.0000, 1.2000, 0.0000, 1.1281, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000},
-          {0.0000, 0.0000, 1.1281, 0.0000, 2.7000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000},
-          {0.0000, 0.0000, 0.0000, 2.7000, 0.0000, 0.0000, 0.0000, 0.0000, 1.7000, 0.0000, 0.0000},
-          {3.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.5232, 0.0000, 0.0000, 0.0000, 0.0000},
-          {0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.5232, 0.0000, 1.2000, 0.0000, 0.0000, 0.0000},
-          {0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.2000, 0.0000, 1.1402, 0.0000, 0.0000},
-          {0.0000, 0.0000, 0.0000, 0.0000, 1.7000, 0.0000, 0.0000, 1.1402, 0.0000, 0.0000, 0.0000},
-          emptyRow.clone(),
-          emptyRow.clone()
-        };
+    public static final double[][] origGraph = new double[][] {
+        { 0.0000, 1.4431, 0.0000, 0.0000, 0.0000, 3.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 },
+        { 1.4431, 0.0000, 1.2000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 },
+        { 0.0000, 1.2000, 0.0000, 1.1281, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 },
+        { 0.0000, 0.0000, 1.1281, 0.0000, 2.7000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000 },
+        { 0.0000, 0.0000, 0.0000, 2.7000, 0.0000, 0.0000, 0.0000, 0.0000, 1.7000, 0.0000, 0.0000 },
+        { 3.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.5232, 0.0000, 0.0000, 0.0000, 0.0000 },
+        { 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.5232, 0.0000, 1.2000, 0.0000, 0.0000, 0.0000 },
+        { 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 0.0000, 1.2000, 0.0000, 1.1402, 0.0000, 0.0000 },
+        { 0.0000, 0.0000, 0.0000, 0.0000, 1.7000, 0.0000, 0.0000, 1.1402, 0.0000, 0.0000, 0.0000 },
+        emptyRow.clone(),
+        emptyRow.clone()
+    };
 
     public static final PathConstraints dynamicPathConstraints = new PathConstraints(5, 5);
   }
