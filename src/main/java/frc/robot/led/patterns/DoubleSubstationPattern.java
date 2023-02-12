0e361b59eff565d46bae7869eb15ffe6b6d31231
jerryfan <jerryfan5433@gmail.com>
2023-02-11 16:01:39 -0800

    Added substation command for auto to intake the cone(might have to do cube later) and then move back a bit. Added one todo for the fixed SetArmAngle

---
+++ src/main/java/frc/robot/led/patterns/DoubleSubstationPattern.java
@@ -0,0 +1,21 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.robot.led.patterns;
+
+import edu.wpi.first.wpilibj.util.Color;
+import frc.robot.led.patternBases.LEDPattern;
+
+import static frc.robot.led.LEDConstants.kResolution;
+
+/** Full Yellow */
+public class DoubleSubstationPattern extends LEDPattern {
+  public DoubleSubstationPattern() {
+    super();
+    setPixelRange(1, kResolution, Color.kGreen);
+  }
+}
