04ab6222c298e42e804e55d71017ab5251507b49
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-02-18 00:07:58 -0800

    Remove swreve from robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.RobotMode;
-import frc.robot.swerve.SwerveDrive;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -27,7 +26,6 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 public class Robot extends LoggedRobot {
   private Command autonomousCommand;
   private RobotContainer robotContainer;
-  private SwerveDrive swerveDrive;
 
   /**
    * This function is run when the robot is first started up and should be used for any
