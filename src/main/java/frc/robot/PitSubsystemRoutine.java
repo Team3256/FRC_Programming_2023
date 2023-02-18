113c843f13ce8fe6565025273af0e5e863ce5c93
Smogless <ryan.niu@warriorlife.net>
2023-02-18 11:53:08 -0800

    merging with main

--- src/main/java/frc/robot/PitSubsystemRoutine.java
+++ src/main/java/frc/robot/PitSubsystemRoutine.java
@@ -1,4 +1,15 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot;
+
+import static frc.robot.Constants.*;
+import static frc.robot.swerve.SwerveConstants.kFieldRelative;
+import static frc.robot.swerve.SwerveConstants.kOpenLoop;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -14,68 +25,74 @@ import frc.robot.swerve.commands.TeleopSwerve;
 import frc.robot.swerve.commands.TeleopSwerveLimited;
 import frc.robot.swerve.commands.TeleopSwerveWithAzimuth;
 
-import static frc.robot.Constants.*;
-import static frc.robot.swerve.SwerveConstants.kFieldRelative;
-import static frc.robot.swerve.SwerveConstants.kOpenLoop;
-
 public class PitSubsystemRoutine {
-    Elevator elevatorSubsystem;
-    Intake intakeSubsystem;
-    SwerveDrive swerveSubsystem;
-    private final CommandXboxController driver = new CommandXboxController(0);
+  Elevator elevatorSubsystem;
+  Intake intakeSubsystem;
+  SwerveDrive swerveSubsystem;
+  private final CommandXboxController driver = new CommandXboxController(0);
 
-    public PitSubsystemRoutine(Elevator elevator, Intake intake, SwerveDrive swerve) {
-        elevatorSubsystem = elevator;
-        intakeSubsystem = intake;
-        swerveSubsystem = swerve;
+  public PitSubsystemRoutine(Elevator elevator, Intake intake, SwerveDrive swerve) {
+    elevatorSubsystem = elevator;
+    intakeSubsystem = intake;
+    swerveSubsystem = swerve;
+  }
+
+  public void pitRoutine() {
+    if (kElevatorEnabled) {
+      elevatorCommands();
     }
-    public void pitRoutine() {
-        if (kElevatorEnabled) {
-            elevatorCommands();
-        }
-        if (kIntakeEnabled) {
-            intakeCommands();
-        }
-        if (kSwerveEnabled) {
-            swerveCommands();
-        }
+    if (kIntakeEnabled) {
+      intakeCommands();
     }
-    public void intakeCommands() {
-        Command zeroElevator = new ZeroElevator(elevatorSubsystem);
-        zeroElevator.initialize();
-        Command setElevatorHeight = new SetElevatorHeight(elevatorSubsystem, 1);
-        setElevatorHeight.initialize();
+    if (kSwerveEnabled) {
+      swerveCommands();
     }
-    public void elevatorCommands() {
-        Command intakeCone = new IntakeCone(intakeSubsystem);
-        intakeCone.initialize();
-        Command intakeCube = new IntakeCube(intakeSubsystem);
-        intakeCube.initialize();
-    }
-    public void swerveCommands() {
-        LockSwerve lockSwerve = new LockSwerve(swerveSubsystem);
-        lockSwerve.initialize();
-        TeleopSwerve teleopSwerve = new TeleopSwerve(swerveSubsystem,
-                () -> driver.getLeftY(),
-                () -> driver.getLeftX(),
-                () -> driver.getRightX(),
-                kFieldRelative,
-                kOpenLoop);
-        teleopSwerve.initialize();
-        TeleopSwerveLimited teleopSwerveLimited = new TeleopSwerveLimited(swerveSubsystem,
-                () -> driver.getRightY(),
-                () -> driver.getRightX(),
-                () -> driver.getLeftX(),
-                kFieldRelative,
-                kOpenLoop);
-        teleopSwerveLimited.initialize();
-        TeleopSwerveWithAzimuth teleopSwerveWithAzimuth = new TeleopSwerveWithAzimuth(swerveSubsystem,
-                () -> driver.getRightY(),
-                () -> driver.getRightX(),
-                () -> driver.getLeftX(),
-                () -> driver.getLeftY(),
-                kFieldRelative,
-                kOpenLoop);
-        teleopSwerveWithAzimuth.initialize();
-    }
+  }
+
+  public void intakeCommands() {
+    Command zeroElevator = new ZeroElevator(elevatorSubsystem);
+    zeroElevator.initialize();
+    Command setElevatorHeight = new SetElevatorHeight(elevatorSubsystem, 1);
+    setElevatorHeight.initialize();
+  }
+
+  public void elevatorCommands() {
+    Command intakeCone = new IntakeCone(intakeSubsystem);
+    intakeCone.initialize();
+    Command intakeCube = new IntakeCube(intakeSubsystem);
+    intakeCube.initialize();
+  }
+
+  public void swerveCommands() {
+    LockSwerve lockSwerve = new LockSwerve(swerveSubsystem);
+    lockSwerve.initialize();
+    TeleopSwerve teleopSwerve =
+        new TeleopSwerve(
+            swerveSubsystem,
+            () -> driver.getLeftY(),
+            () -> driver.getLeftX(),
+            () -> driver.getRightX(),
+            kFieldRelative,
+            kOpenLoop);
+    teleopSwerve.initialize();
+    TeleopSwerveLimited teleopSwerveLimited =
+        new TeleopSwerveLimited(
+            swerveSubsystem,
+            () -> driver.getRightY(),
+            () -> driver.getRightX(),
+            () -> driver.getLeftX(),
+            kFieldRelative,
+            kOpenLoop);
+    teleopSwerveLimited.initialize();
+    TeleopSwerveWithAzimuth teleopSwerveWithAzimuth =
+        new TeleopSwerveWithAzimuth(
+            swerveSubsystem,
+            () -> driver.getRightY(),
+            () -> driver.getRightX(),
+            () -> driver.getLeftX(),
+            () -> driver.getLeftY(),
+            kFieldRelative,
+            kOpenLoop);
+    teleopSwerveWithAzimuth.initialize();
+  }
 }
--- src/main/java/frc/robot/intake/IntakeConstants.java
+++ src/main/java/frc/robot/intake/IntakeConstants.java
@@ -7,8 +7,13 @@
 
 package frc.robot.intake;
 
+import frc.robot.drivers.CanDeviceId;
+
 public final class IntakeConstants {
-  public static final int kIntakeMotorID = 14;
+  public static final int kIntakeMotorID = 4;
+  public static final String kIntakeCANBus = "mani";
+  public static final CanDeviceId kIntakeCANDevice = new CanDeviceId(kIntakeMotorID, kIntakeCANBus);
+
   public static final double kIntakeConeSpeed = 0.5;
   public static final double kIntakeCubeSpeed = -0.5;
 }
