31e0af215c9eeb5103c75b54a2256ccac5bc974d
Smogless <92347357+Smogless@users.noreply.github.com>
2023-02-18 00:02:26 -0800

    made so autos will switch based on alliance color (#145)
    * made so autos will switch based on alliance color
    
    * added firstSegment checker in the AutoBuilder and PPTrajectoryCommand to check if segment is first in auto path
    
    * Run spotless
    
    * resolved issues on PR
    
    * Rename create command function
    
    * Automatically set first segment in path group
    
    * Fixs
    
    * Run spotless
    
    * renamed variables
    
    * Delete networktables.json
    
    ---------
    
    Co-authored-by: Aditya Pawar <34043950+adityapawar1@users.noreply.github.com>
    Co-authored-by: Aditya Pawar <aditya.pawar@warriorlife.net>
    Co-authored-by: vivek nadig <vivek.nadig@warriorlife.net>
    Co-authored-by: Vivek Nadig <54611298+Savimaster@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.swerve.SwerveDrive;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -30,6 +31,7 @@ public class Robot extends LoggedRobot {
   private Command autonomousCommand;
 
   private RobotContainer robotContainer;
+  private SwerveDrive swerveDrive;
 
   /**
    * This function is run when the robot is first started up and should be used for any
