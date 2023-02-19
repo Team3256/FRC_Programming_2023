0857d3b1d8dcce5abc7cb978aad70d43795c48bf
3LucasZ <lucas.zheng@warriorlife.net>
2023-02-18 21:04:23 -0800

    Update PathUtil.java

--- src/main/java/frc/robot/auto/dynamicpathgeneration/helpers/PathUtil.java
+++ src/main/java/frc/robot/auto/dynamicpathgeneration/helpers/PathUtil.java
@@ -8,14 +8,10 @@
 package frc.robot.auto.dynamicpathgeneration.helpers;
 
 import static frc.robot.auto.dynamicpathgeneration.DynamicPathConstants.*;
-import static frc.robot.auto.dynamicpathgeneration.DynamicPathFinder.doesPathSegmentHitObstacles;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.swerve.SwerveConstants;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 
 public class PathUtil{
   public static double splineHeuristic(Translation2d position1, Translation2d position2) {
@@ -27,5 +23,27 @@ public class PathUtil{
     double estimatedTime = splineHeuristic(position1, position2);
     if (doesPathSegmentHitObstacles(position1, position2)) estimatedTime += ILLEGAL_TIME;
     return estimatedTime;
+  }
+
+  public static boolean doesLineHitObstacles(Translation2d position1, Translation2d position2) {
+    for (Obstacle obstacle : obstacles) {
+      if (obstacle.intersectsLineSegment(position1, position2)) return true;
+    }
+    return false;
+  }
+
+  public static boolean doesPathSegmentHitObstacles(
+      Translation2d position1, Translation2d position2) {
+    if (doesLineHitObstacles(position1, position2)) return true;
+
+    Rotation2d normalAngle = position2.minus(position1).getAngle().plus(Rotation2d.fromDegrees(90));
+    Translation2d normalVector = new Translation2d(1, 0).rotateBy(normalAngle).times(kRobotRadius);
+
+    if (doesLineHitObstacles(position1.minus(normalVector), position2.minus(normalVector)))
+      return true;
+    if (doesLineHitObstacles(position1.plus(normalVector), position2.plus(normalVector)))
+      return true;
+
+    return false;
   }
 }
