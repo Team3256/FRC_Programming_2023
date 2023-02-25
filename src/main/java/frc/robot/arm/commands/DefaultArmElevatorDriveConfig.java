e66624c7ca29c32f30a36acffb7fa95852008333
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-02-24 20:35:56 -0800

    Idk

--- src/main/java/frc/robot/arm/commands/DefaultArmElevatorDriveConfig.java
+++ src/main/java/frc/robot/arm/commands/DefaultArmElevatorDriveConfig.java
@@ -8,17 +8,15 @@
 package frc.robot.arm.commands;
 
 import static frc.robot.arm.ArmConstants.kDefaultArmAngle;
-import static frc.robot.elevator.ElevatorConstants.kMinHeight;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.arm.Arm;
 import frc.robot.elevator.Elevator;
-import frc.robot.elevator.commands.SetElevatorHeight;
+import frc.robot.elevator.commands.ZeroElevator;
 
 public class DefaultArmElevatorDriveConfig extends ParallelCommandGroup {
   public DefaultArmElevatorDriveConfig(Elevator elevatorSubsystem, Arm armSubsystem) {
     addCommands(
-        new SetElevatorHeight(elevatorSubsystem, kMinHeight),
-        new SetArmAngle(armSubsystem, kDefaultArmAngle));
+        new ZeroElevator(elevatorSubsystem), new SetArmAngle(armSubsystem, kDefaultArmAngle));
   }
 }
