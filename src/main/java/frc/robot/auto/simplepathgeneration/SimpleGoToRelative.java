a6516c13be4d51466bae12093c295267577db4c2
3LucasZ <lucas.zheng@warriorlife.net>
2023-03-10 22:57:27 -0800

    FixSimpleGoToSubstation

--- src/main/java/frc/robot/auto/simplepathgeneration/SimpleGoToRelative.java
+++ src/main/java/frc/robot/auto/simplepathgeneration/SimpleGoToRelative.java
@@ -1,3 +1,10 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.auto.simplepathgeneration;
 
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -6,9 +13,9 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.swerve.SwerveDrive;
 
-
 public class SimpleGoToRelative {
-    static Command run(SwerveDrive swerveDrive, Translation2d translation2d){
-        return SimpleGoToAbsolute.run(swerveDrive, swerveDrive.getPose().plus(new Transform2d(translation2d, new Rotation2d(0))));
-    }
+  static Command run(SwerveDrive swerveDrive, Translation2d translation2d) {
+    return SimpleGoToAbsolute.run(
+        swerveDrive, swerveDrive.getPose().plus(new Transform2d(translation2d, new Rotation2d(0))));
+  }
 }
--- src/main/java/frc/robot/auto/simplepathgeneration/SimpleGoToSubstation.java
+++ src/main/java/frc/robot/auto/simplepathgeneration/SimpleGoToSubstation.java
@@ -1,39 +1,66 @@
+// Copyright (c) 2023 FRC 3256
+// https://github.com/Team3256
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot.auto.simplepathgeneration;
+
+import static frc.robot.auto.dynamicpathgeneration.DynamicPathConstants.kBlueTopDoubleSubstationPose;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.arm.Arm;
 import frc.robot.arm.commands.SetArmAngle;
-import frc.robot.auto.dynamicpathgeneration.helpers.GeometryUtil;
 import frc.robot.auto.dynamicpathgeneration.helpers.PathUtil;
+import frc.robot.elevator.Elevator;
 import frc.robot.elevator.commands.SetElevatorHeight;
+import frc.robot.intake.Intake;
+import frc.robot.intake.commands.IntakeCone;
+import frc.robot.intake.commands.IntakeCube;
 import frc.robot.led.LED;
 import frc.robot.led.commands.LEDSetAllSectionsPattern;
 import frc.robot.led.patterns.DynamicPathGenSuccessPattern;
 import frc.robot.swerve.SwerveDrive;
-import static frc.robot.auto.dynamicpathgeneration.DynamicPathConstants.kBlueTopDoubleSubstationPose;
-public class SimpleGoToSubstation {
-    static Command run(SwerveDrive swerveDrive, LED ledSubsystem, boolean isCurrentPieceCone){
-        Pose2d sink = DriverStation.getAlliance()==Alliance.Blue?kBlueTopDoubleSubstationPose : PathUtil.flip(kBlueTopDoubleSubstationPose);
-        Command moveCommand = SimpleGoToAbsolute.run(swerveDrive, sink);
-        Command intakeCommand = new ParallelCommandGroup(
-            new ConditionalCommand(
-                new SetElevatorHeight(elevatorSubsystem, ElevatorPosition.CONE_HIGH),
-                new SetElevatorHeight(elevatorSubsystem, ElevatorPosition.CUBE_HIGH),
-                ()->isCurrentPieceCone),
-            new ConditionalCommand(
-                new SetArmAngle(armSubsystem, ArmPosition.CONE_HIGH),
-                new SetArmAngle(armSubsystem, ArmPosition.CUBE_HIGH),
-                ()->isCurrentPieceCone));
-        Command moveBackCommand = SimpleGoToRelative.run(swerveDrive, new Translation2d(0,-0.25));
-        LEDSetAllSectionsPattern flashCommand = new LEDSetAllSectionsPattern(ledSubsystem, new DynamicPathGenSuccessPattern());
 
-        Command finalCommand = new SequentialCommandGroup(moveCommand, intakeCommand, moveBackCommand, flashCommand);
-        return finalCommand;
-    }
+public class SimpleGoToSubstation {
+  static Command run(
+      SwerveDrive swerveDrive,
+      Intake intakeSubsystem,
+      Elevator elevatorSubsystem,
+      Arm armSubsystem,
+      LED ledSubsystem,
+      boolean isCurrentPieceCone) {
+    Pose2d sink =
+        DriverStation.getAlliance() == Alliance.Blue
+            ? kBlueTopDoubleSubstationPose
+            : PathUtil.flip(kBlueTopDoubleSubstationPose);
+    Command moveCommand = SimpleGoToAbsolute.run(swerveDrive, sink);
+    Command moveArmAndElevatorCommand =
+        new ParallelCommandGroup(
+            new ConditionalCommand(
+                new SetElevatorHeight(elevatorSubsystem, Elevator.ElevatorPosition.CONE_HIGH),
+                new SetElevatorHeight(elevatorSubsystem, Elevator.ElevatorPosition.CUBE_HIGH),
+                () -> isCurrentPieceCone),
+            new ConditionalCommand(
+                new SetArmAngle(armSubsystem, Arm.ArmPosition.CONE_HIGH),
+                new SetArmAngle(armSubsystem, Arm.ArmPosition.CUBE_HIGH),
+                () -> isCurrentPieceCone));
+    Command intakeCommand =
+        new ConditionalCommand(
+            new IntakeCone(intakeSubsystem),
+            new IntakeCube(intakeSubsystem),
+            () -> isCurrentPieceCone);
+    Command moveBackCommand = SimpleGoToRelative.run(swerveDrive, new Translation2d(0, -0.25));
+    LEDSetAllSectionsPattern flashCommand =
+        new LEDSetAllSectionsPattern(ledSubsystem, new DynamicPathGenSuccessPattern());
+
+    Command finalCommand =
+        new SequentialCommandGroup(moveCommand, intakeCommand, moveBackCommand, flashCommand);
+    return finalCommand;
+  }
 }
