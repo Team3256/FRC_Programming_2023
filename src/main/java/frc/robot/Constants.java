abb08c2cfd062d08633737fbe64d8a39e9f63de8
Vivek Nadig <54611298+Savimaster@users.noreply.github.com>
2023-03-25 10:52:38 -0700

    fixed intake and outtake with latching (#222)
    * fixed intake and outtake with latching
    
    * Changed to run latching once a gamepiece entes intake

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,11 +19,11 @@ import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import java.util.Map;
 
 public final class Constants {
-  public static final boolean kDebugEnabled = true;
+  public static final boolean kDebugEnabled = false;
 
-  public static final boolean kSwerveEnabled = false;
-  public static final boolean kIntakeEnabled = false;
-  public static final boolean kElevatorEnabled = false;
+  public static final boolean kSwerveEnabled = true;
+  public static final boolean kIntakeEnabled = true;
+  public static final boolean kElevatorEnabled = true;
   public static final boolean kArmEnabled = true;
   public static final boolean kLedStripEnabled = true;
 
