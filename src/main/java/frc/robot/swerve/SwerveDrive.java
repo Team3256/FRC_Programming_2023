72a10d244497c5855f684777e9ceea306b55c980
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-03-31 21:58:11 -0600

    Run spotless

--- src/main/java/frc/robot/swerve/SwerveDrive.java
+++ src/main/java/frc/robot/swerve/SwerveDrive.java
@@ -35,7 +35,6 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.Constants.FeatureFlags;
 import frc.robot.drivers.CANDeviceTester;
 import frc.robot.drivers.CANTestable;
@@ -56,13 +55,13 @@ public class SwerveDrive extends SubsystemBase implements Loggable, CANTestable 
   private final Field2d field = new Field2d();
   private final Field2d limelightLocalizationField = new Field2d();
 
-  private final AdaptiveSlewRateLimiter adaptiveXRateLimiter = new AdaptiveSlewRateLimiter(kXAccelRateLimit,
-      kXDecelRateLimit);
-  private final AdaptiveSlewRateLimiter adaptiveYRateLimiter = new AdaptiveSlewRateLimiter(kYAccelRateLimit,
-      kYDecelRateLimit);
+  private final AdaptiveSlewRateLimiter adaptiveXRateLimiter =
+      new AdaptiveSlewRateLimiter(kXAccelRateLimit, kXDecelRateLimit);
+  private final AdaptiveSlewRateLimiter adaptiveYRateLimiter =
+      new AdaptiveSlewRateLimiter(kYAccelRateLimit, kYDecelRateLimit);
 
   private final SwerveModule[] swerveModules = {
-      frontLeftModule, frontRightModule, backLeftModule, backRightModule
+    frontLeftModule, frontRightModule, backLeftModule, backRightModule
   };
 
   private final Pigeon2 gyro;
@@ -72,18 +71,19 @@ public class SwerveDrive extends SubsystemBase implements Loggable, CANTestable 
     gyro.configFactoryDefault();
     zeroGyroYaw();
 
-    poseEstimator = new SwerveDrivePoseEstimator(
-        kSwerveKinematics,
-        getYaw(),
-        new SwerveModulePosition[] {
-            frontLeftModule.getPosition(),
-            frontRightModule.getPosition(),
-            backLeftModule.getPosition(),
-            backRightModule.getPosition()
-        },
-        new Pose2d(),
-        new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.9, 0.9, 0.02), // Current state X, Y, theta.
-        new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.10, 0.10, 0.04));
+    poseEstimator =
+        new SwerveDrivePoseEstimator(
+            kSwerveKinematics,
+            getYaw(),
+            new SwerveModulePosition[] {
+              frontLeftModule.getPosition(),
+              frontRightModule.getPosition(),
+              backLeftModule.getPosition(),
+              backRightModule.getPosition()
+            },
+            new Pose2d(),
+            new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.9, 0.9, 0.02), // Current state X, Y, theta.
+            new MatBuilder<>(Nat.N3(), Nat.N1()).fill(0.10, 0.10, 0.04));
 
     if (kDebugEnabled) {
       SmartDashboard.putData("Limelight Localization Field", limelightLocalizationField);
@@ -106,20 +106,25 @@ public class SwerveDrive extends SubsystemBase implements Loggable, CANTestable 
 
   public void drive(ChassisSpeeds chassisSpeeds, boolean isOpenLoop) {
     if (FeatureFlags.kSwerveAccelerationLimitingEnabled) {
-      chassisSpeeds.vxMetersPerSecond = adaptiveXRateLimiter.calculate(chassisSpeeds.vxMetersPerSecond);
-      chassisSpeeds.vyMetersPerSecond = adaptiveYRateLimiter.calculate(chassisSpeeds.vyMetersPerSecond);
+      chassisSpeeds.vxMetersPerSecond =
+          adaptiveXRateLimiter.calculate(chassisSpeeds.vxMetersPerSecond);
+      chassisSpeeds.vyMetersPerSecond =
+          adaptiveYRateLimiter.calculate(chassisSpeeds.vyMetersPerSecond);
     }
-    Pose2d robotPoseVelocity = new Pose2d(
-        chassisSpeeds.vxMetersPerSecond * kPeriodicDeltaTime,
-        chassisSpeeds.vyMetersPerSecond * kPeriodicDeltaTime,
-        Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond * kPeriodicDeltaTime));
+    Pose2d robotPoseVelocity =
+        new Pose2d(
+            chassisSpeeds.vxMetersPerSecond * kPeriodicDeltaTime,
+            chassisSpeeds.vyMetersPerSecond * kPeriodicDeltaTime,
+            Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond * kPeriodicDeltaTime));
     Twist2d twistVelocity = (new Pose2d()).log(robotPoseVelocity);
-    ChassisSpeeds updatedChassisSpeeds = new ChassisSpeeds(
-        twistVelocity.dx / kPeriodicDeltaTime,
-        twistVelocity.dy / kPeriodicDeltaTime,
-        twistVelocity.dtheta / kPeriodicDeltaTime);
+    ChassisSpeeds updatedChassisSpeeds =
+        new ChassisSpeeds(
+            twistVelocity.dx / kPeriodicDeltaTime,
+            twistVelocity.dy / kPeriodicDeltaTime,
+            twistVelocity.dtheta / kPeriodicDeltaTime);
 
-    SwerveModuleState[] swerveModuleStates = kSwerveKinematics.toSwerveModuleStates(updatedChassisSpeeds);
+    SwerveModuleState[] swerveModuleStates =
+        kSwerveKinematics.toSwerveModuleStates(updatedChassisSpeeds);
 
     SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, kMaxSpeed);
 
@@ -131,10 +136,11 @@ public class SwerveDrive extends SubsystemBase implements Loggable, CANTestable 
 
   public void drive(
       Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
-    ChassisSpeeds swerveChassisSpeed = fieldRelative
-        ? ChassisSpeeds.fromFieldRelativeSpeeds(
-            translation.getX(), translation.getY(), rotation, getYaw())
-        : new ChassisSpeeds(translation.getX(), translation.getY(), rotation);
+    ChassisSpeeds swerveChassisSpeed =
+        fieldRelative
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(
+                translation.getX(), translation.getY(), rotation, getYaw())
+            : new ChassisSpeeds(translation.getX(), translation.getY(), rotation);
 
     drive(swerveChassisSpeed, isOpenLoop);
   }
@@ -172,13 +178,11 @@ public class SwerveDrive extends SubsystemBase implements Loggable, CANTestable 
 
   public void zeroGyroYaw() {
     gyro.setYaw(0);
-    if (kDebugEnabled)
-      System.out.println("Resetting Gyro");
+    if (kDebugEnabled) System.out.println("Resetting Gyro");
   }
 
   public void setGyroYaw(double yawDegrees) {
-    if (kDebugEnabled)
-      System.out.println("Setting gyro yaw to: " + yawDegrees);
+    if (kDebugEnabled) System.out.println("Setting gyro yaw to: " + yawDegrees);
     gyro.setYaw(yawDegrees);
   }
 
