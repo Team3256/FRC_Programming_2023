d5e3ce83b765c75d7d43974c37f739715474f201
samuel <116925676+memori034@users.noreply.github.com>
2023-03-31 17:34:03 -0700

    Add incomplete IntakeFromGroundCommand

---
+++ src/main/java/frc/robot/intake/commands/IntakeFromGround.java
@@ -0,0 +1,23 @@
+package frc.robot.intake.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.arm.Arm;
+import frc.robot.arm.commands.SetArmAngle;
+import frc.robot.elevator.Elevator;
+import frc.robot.elevator.commands.ZeroElevator;
+
+public class IntakeFromGround extends SequentialCommandGroup {
+    //Move the elevator and arm to its lowest position 
+    //Then, apply current to slightly push the intake into the ground
+
+    public IntakeFromGround(Elevator elevatorSubsystem, Arm armSubsystem) {
+        addCommands(
+            new WaitCommand(0),
+            new ZeroElevator(elevatorSubsystem),
+            new SetArmAngle(armSubsystem, Arm.ArmPreset.kAnyPieceLowRotation),
+            new 
+        );
+    }
+}
