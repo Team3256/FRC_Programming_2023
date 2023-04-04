684f60dbcbf10ac563d81f18f1073b7aac0864aa
vivek nadig <vivek.nadig@warriorlife.net>
2023-04-03 21:47:31 -0700

    Removing timeouts

--- src/main/java/frc/robot/auto/commands/SetArmElevatorStart.java
+++ src/main/java/frc/robot/auto/commands/SetArmElevatorStart.java
@@ -19,7 +19,6 @@ public class SetArmElevatorStart extends ParallelCommandGroup {
   public SetArmElevatorStart(Elevator elevatorSubsystem, Arm armSubsystem) {
     addCommands(
         new SetArmAngle(armSubsystem, Rotation2d.fromDegrees(180))
-            .withTimeout(1.25)
             .andThen(new ZeroElevator(elevatorSubsystem))
             .andThen(new SetArmAngle(armSubsystem, ArmPreset.DEFAULT)));
   }
