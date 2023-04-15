c1192c011bf9f13a3181f814f2457576b7956bb0
Aditya Pawar <aditya.pawar@warriorlife.net>
2023-04-15 03:59:43 -0700

    Add toggle game piece on one button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -47,9 +47,12 @@ import frc.robot.swerve.commands.TeleopSwerveWithAzimuth;
 import java.util.ArrayList;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer implements CANTestable, Loggable {
@@ -72,7 +75,7 @@ public class RobotContainer implements CANTestable, Loggable {
   private Elevator elevatorSubsystem;
   private Arm armSubsystem;
   private LED ledSubsystem;
-  private GamePiece currentPiece = GamePiece.CONE;
+  private GamePiece currentPiece = GamePiece.CUBE;
   private SubstationLocation doubleSubstationLocation = SubstationLocation.RIGHT_SIDE;
   private RobotSimulation robotSimulation;
   private SendableChooser<Mode> modeChooser;
@@ -83,11 +86,16 @@ public class RobotContainer implements CANTestable, Loggable {
   private final ArrayList<Loggable> loggables = new ArrayList<Loggable>();
 
   public RobotContainer() {
-    if (kArmEnabled) armSubsystem = new Arm();
-    if (kIntakeEnabled) intakeSubsystem = new Intake();
-    if (kElevatorEnabled) elevatorSubsystem = new Elevator();
-    if (kSwerveEnabled) swerveSubsystem = new SwerveDrive();
-    if (kLedStripEnabled) ledSubsystem = new LED();
+    if (kArmEnabled)
+      armSubsystem = new Arm();
+    if (kIntakeEnabled)
+      intakeSubsystem = new Intake();
+    if (kElevatorEnabled)
+      elevatorSubsystem = new Elevator();
+    if (kSwerveEnabled)
+      swerveSubsystem = new SwerveDrive();
+    if (kLedStripEnabled)
+      ledSubsystem = new LED();
 
     if (kLedStripEnabled) {
       configureLEDStrip();
@@ -125,13 +133,12 @@ public class RobotContainer implements CANTestable, Loggable {
     SmartDashboard.putString(
         "Current Double Substation Location", doubleSubstationLocation.toString());
 
-    autoPaths =
-        new AutoPaths(
-            swerveSubsystem,
-            intakeSubsystem,
-            elevatorSubsystem,
-            armSubsystem,
-            this::isCurrentPieceCone);
+    autoPaths = new AutoPaths(
+        swerveSubsystem,
+        intakeSubsystem,
+        elevatorSubsystem,
+        armSubsystem,
+        this::isCurrentPieceCone);
     autoPaths.sendCommandsToChooser();
 
     if (AutoConstants.kAutoDebug) {
@@ -145,8 +152,7 @@ public class RobotContainer implements CANTestable, Loggable {
     }
 
     if (RobotBase.isSimulation()) {
-      robotSimulation =
-          new RobotSimulation(swerveSubsystem, intakeSubsystem, armSubsystem, elevatorSubsystem);
+      robotSimulation = new RobotSimulation(swerveSubsystem, intakeSubsystem, armSubsystem, elevatorSubsystem);
       robotSimulation.initializeRobot();
       robotSimulation.addDoubleSubstation(GamePiece.CONE);
     }
@@ -220,12 +226,12 @@ public class RobotContainer implements CANTestable, Loggable {
         .leftBumper()
         .toggleOnTrue(
             new TeleopSwerveLimited(
-                    swerveSubsystem,
-                    driver::getLeftY,
-                    driver::getLeftX,
-                    driver::getRightX,
-                    kFieldRelative,
-                    kOpenLoop)
+                swerveSubsystem,
+                driver::getLeftY,
+                driver::getLeftX,
+                driver::getRightX,
+                kFieldRelative,
+                kOpenLoop)
                 .deadlineWith(new LimitedSwervePattern(ledSubsystem, this::isCurrentPieceCone)));
 
     driver
@@ -322,14 +328,11 @@ public class RobotContainer implements CANTestable, Loggable {
   public void configureLEDStrip() {
     ledSubsystem.setDefaultCommand(new ColorFlowPattern(ledSubsystem));
 
+    updateGamePieceLEDs();
     operator
         .rightBumper()
         .toggleOnTrue(
-            new InstantCommand(this::setPieceToCone).andThen(new SetAllBlink(ledSubsystem, kCone)));
-    operator
-        .leftBumper()
-        .toggleOnTrue(
-            new InstantCommand(this::setPieceToCube).andThen(new SetAllBlink(ledSubsystem, kCube)));
+            new InstantCommand(this::toggleGamePiece).andThen(new InstantCommand(this::updateGamePieceLEDs)));
   }
 
   public Command getAutonomousCommand() {
@@ -380,14 +383,15 @@ public class RobotContainer implements CANTestable, Loggable {
   public boolean CANTest() {
     System.out.println("Testing CAN connections:");
     boolean result = true;
-    for (CANTestable subsystem : canBusTestables) result &= subsystem.CANTest();
+    for (CANTestable subsystem : canBusTestables)
+      result &= subsystem.CANTest();
     System.out.println("CAN fully connected: " + result);
     return result;
   }
 
   public void startPitRoutine() {
-    PitTestRoutine pitSubsystemRoutine =
-        new PitTestRoutine(elevatorSubsystem, intakeSubsystem, swerveSubsystem, armSubsystem);
+    PitTestRoutine pitSubsystemRoutine = new PitTestRoutine(elevatorSubsystem, intakeSubsystem, swerveSubsystem,
+        armSubsystem);
     pitSubsystemRoutine.runPitRoutine();
   }
 
@@ -395,12 +399,20 @@ public class RobotContainer implements CANTestable, Loggable {
     return GamePiece.CONE.equals(currentPiece);
   }
 
-  public void setPieceToCone() {
-    currentPiece = GamePiece.CONE;
+  public void toggleGamePiece() {
+    if (currentPiece == GamePiece.CONE) {
+      currentPiece = GamePiece.CUBE;
+    } else {
+      currentPiece = GamePiece.CONE;
+    }
   }
 
-  public void setPieceToCube() {
-    currentPiece = GamePiece.CUBE;
+  public void updateGamePieceLEDs() {
+    if (currentPiece == GamePiece.CONE) {
+      new SetAllBlink(ledSubsystem, kCone).schedule();
+    } else {
+      new SetAllBlink(ledSubsystem, kCube).schedule();
+    }
   }
 
   public GamePiece getCurrentPiece() {
