ef7fd92e351649db31f4ddd5319496ef9934d442
Ryan Abraham <ryan.ryanabraham@gmail.com>
2023-09-18 17:41:41 -0700

    Changed default led color in LEDconstants.java to blue from orange

--- src/main/java/frc/robot/led/LEDConstants.java
+++ src/main/java/frc/robot/led/LEDConstants.java
@@ -16,5 +16,5 @@ public class LEDConstants {
   public static final Color kLockSwerve = Color.green;
   public static final Color kSuccess = Color.green;
   public static final Color kError = Color.red;
-  public static final Color kDefault = Color.orange;
+  public static final Color kDefault = Color.blue;
 }
